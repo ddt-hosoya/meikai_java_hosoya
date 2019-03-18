69e38caed75e18df085768777f8d065cda4651b3
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-03-18 20:46:34 +0900

    2019/03/18_演習1ー1_1回目_進捗率100％

---
+++ meikai_java/src/e01/e01_01.java
@@ -0,0 +1,13 @@
+package e01;
+/*
+演習1－1
+プログラム中の文の終端を示すセミコロン；が欠如してるとどうなるのか。
+プログラムをコンパイルして検証せよ。
+ */
+public class e01_01 {
+	public static void main(String[] args){
+		System.out.println("はじめてのJavaプログラム");
+		System.out.println("画面に出力しています。")
+	}
+
+}
