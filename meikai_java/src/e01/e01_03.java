48b752fb11a452dc10c84297c90578a3abdae53b
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-03-20 20:13:03 +0900

    2019/03/20_演習1-3_1回目_進捗率100％

---
+++ meikai_java/src/e01/e01_03.java
@@ -0,0 +1,15 @@
+package e01;
+/*
+演習1-3
+右に示すように、各行に1行ずつ名前を表示するプログラムを作成せよ。
+姓と名の間は一行あけることとし、自分の名前を表示すること。
+ */
+
+public class e01_03 {
+	public static void main(String[] args){
+		//一文字ずつ改行する
+		//姓と名の間を改行する
+		System.out.println("細\n谷\n\n束\n紗\n");
+	}
+
+}
