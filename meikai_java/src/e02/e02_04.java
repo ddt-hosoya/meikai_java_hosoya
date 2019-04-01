bc0686ccb0391cd2f166ed14c7a59cfac9d8a76b
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-04-01 21:26:30 +0900

    2019/04/01_演習2-6まで_1回目_進捗率100％_変数名とフォーマットを修正

--- meikai_java/src/e02/e02_04.java
+++ meikai_java/src/e02/e02_04.java
@@ -1,24 +1,26 @@
 package e02;
+
 /*
 右に示すように、キーボードから読み込んだ整数値に10を加えた値と
 10を減じた値を出力せよ。
  */
 import java.util.Scanner;
+
 public class e02_04 {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 
-		//文字を表示する。
+		// 文字を表示する。
 		System.out.println("整数値：");
-		//キーボードで読み込んだ値を整数値として宣言する。
+		// キーボードで読み込んだ値を整数値として宣言する。
 		int input = stdIn.nextInt();
-		//加減する値を初期化して宣言する。
+		// 加減する値を初期化して宣言する。
 		int operand = 10;
 
-		//10を加えた値の結果を表示する。
-		System.out.println(operand +"を加えた値は" + (input + operand) + "です。");
-		//10を減じた値の結果を表示する。
-		System.out.println(operand +"を減じた値は" + (input - operand) + "です。");
+		// 10を加えた値の結果を表示する。
+		System.out.println(operand + "を加えた値は" + (input + operand) + "です。");
+		// 10を減じた値の結果を表示する。
+		System.out.println(operand + "を減じた値は" + (input - operand) + "です。");
 	}
 
 }
--- meikai_java/src/e02/e02_05.java
+++ meikai_java/src/e02/e02_05.java
@@ -1,23 +1,25 @@
 package e02;
+
 import java.util.Scanner;
+
 /*
 二つの実数値を読み込み、その和と平均を求めて表示するプログラムを作成せよ。
 */
 public class e02_05 {
-	public static void main(String[] args){
-		Scanner StdIn = new Scanner(System.in);
-		//文字を表示する。
-		System.out.println("xの値：");
-		//キーボードで読み込んだ値を実数値として宣言する。
-		double x = StdIn.nextDouble();
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		// 文字を表示する。
+		System.out.println("1番目の入力値：");
+		// キーボードで読み込んだ値を実数値として宣言する。
+		double inputvalue_first = stdIn.nextDouble();
 
-		//文字を表示する。
-		System.out.println("yの値：");
-		//キーボードで読み込んだ値を実数値として宣言する。
-		double y = StdIn.nextDouble();
+		// 文字を表示する。
+		System.out.println("2番目の入力値：");
+		// キーボードで読み込んだ値を実数値として宣言する。
+		double inputvalue_second = stdIn.nextDouble();
 
-		//合計値を表示する。
-		System.out.println("合計は" + (x + y) + "です。");
-		System.out.println("平均は"+ (x + y)/2 + "です。");
-		}
+		// 合計値を表示する。
+		System.out.println("合計は" + (inputvalue_first + inputvalue_second) + "です。");
+		System.out.println("平均は" + (inputvalue_first + inputvalue_second) / 2 + "です。");
+	}
 }
