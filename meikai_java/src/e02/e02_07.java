ced1ae8b27310ef9fcb886484197a4c464d8fd93
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-04-03 20:06:19 +0900

    2019/04/03_演習2-7_2回目_進捗率100％

--- meikai_java/src/e02/e02_07.java
+++ meikai_java/src/e02/e02_07.java
@@ -9,12 +9,20 @@ import java.util.Random;
 ・2桁の正の整数値（すなわち10以上99以下の値）をランダムに生成して表示。
  */
 public class e02_07 {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Random rand = new Random();
-
-		int positive_int = rand.nextInt();
-
-		System.out.println("1桁の正の整数は：" + positive_int);
+		// 1桁の正の値をランダムに生成して整数型で宣言する。
+		int positive_integer = rand.nextInt(9) + 1;
+		// 値を表示する。
+		System.out.println(positive_integer + "は1桁の正の整数です。");
+		// 1桁の負の値をランダムに生成して整数型で宣言する。
+		int negative_integer = rand.nextInt(9) - 9;
+		// 値を表示する。
+		System.out.println(negative_integer + "は1桁の負の整数です。");
+		// 2桁の正の値をランダムに生成して整数型で宣言する。
+		int double_digit_integer = rand.nextInt(90) + 10;
+		// 値を表示する。
+		System.out.println(double_digit_integer + "は2桁の正の整数です。");
 	}
 
 }
