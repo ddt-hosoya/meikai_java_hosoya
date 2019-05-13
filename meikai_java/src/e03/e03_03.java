0c09ef1a921f86b222ad7e435de86688579eb1c0
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-05-13 20:21:49 +0900

    2019/05/13_演習3-3_1回目_進捗率100％

---
+++ meikai_java/src/e03/e03_03.java
@@ -0,0 +1,25 @@
+package e03;
+
+/*
+List 3-5の最後のelseをelse if (n == 0)に変更したらどうなるかを検討せよ。
+ */
+import java.util.Scanner;
+
+public class e03_03 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("整数値：");
+		int n = stdIn.nextInt();
+
+		if (n > 0)
+			System.out.println("その値は正です。");
+		else if (n < 0)
+			System.out.println("その値は負です。");
+		else if (n == 0)
+			System.out.println("その値は0です。");
+	}
+
+}
+/*
+ * 最後のelseを通る条件はn == 0しか無いためelse if (n == 0)としても 変更前と同等の結果が得られると考えられる。
+ */
