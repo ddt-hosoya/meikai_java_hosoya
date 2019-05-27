91221adf5034670c082d03f721bcc671c0e4c4dc
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-05-27 20:54:58 +0900

    2019/05/27_演習3-5_1回目_進捗率100％

---
+++ meikai_java/src/e03/e03_05.java
@@ -0,0 +1,39 @@
+package e03;
+
+/*
+正の整数値を読み込んで、それが5で割り切れれば『その値は5で割り切れます。』と表示し、
+そうでなければ『その値は5で割り切れません。』と表示するプログラムを作成せよ。
+※正でない値を読み込んだ場合は、『正でない値が入力されました。』と表示すること。
+*/
+import java.util.Scanner;
+
+public class e03_05 {
+	public static void main(String[] args) {
+		// Scannerクラスの標準入力ストリームを実体化する。
+		Scanner stdIn = new Scanner(System.in);
+
+		// 正の整数値：という文字を表示する。
+		System.out.print("正の整数値：");
+		// 入力値をint型で宣言する。
+		int inputInt = stdIn.nextInt();
+		// 割る数5を宣言する。
+		final int adjustInt = 5;
+
+		// 入力された値が正の場合
+		if (inputInt >= 0) {
+			// 5で割り切れる場合
+			if ((inputInt % adjustInt) == 0) {
+				// 『その値は5で割り切れます。』と表示する。
+				System.out.println("その値は5で割り切れます。");
+				// 5で割り切れない場合
+			} else {
+				// 『その値は5で割り切れません。』と表示する。
+				System.out.println("その値は5で割り切れません。");
+			}
+			// 入力された値が正でない場合
+		} else {
+			// 『正でない値が入力されました。』と表示する。
+			System.out.println("正でない値が入力されました。");
+		}
+	}
+}
