6a4b02ad4d68685d562d4885a303deebad12089b
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-03 21:10:30 +0900

    2019/06/03_演習3-9_3回目_進捗率100％_コメントを修正

--- meikai_java/src/e03/e03_09.java
+++ meikai_java/src/e03/e03_09.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 
 public class e03_09 {
 	public static void main(String[] args){
-		// Scannerクラスの標準入力ストリームを実体化する
+		// 入力機能を実体化する
 		Scanner stdIn = new Scanner(System.in);
-		// 実数値：と表示する
+		// ユーザーに対して入力値の制約を表示する
 		System.out.print("実数値：");
-		// 入力値をdouble型で宣言
+		// 入力値を仕様である大きいほうの値かどうかを比較するための変数として代入
 		double inputFirst = stdIn.nextDouble();
-		// 実数値：と表示する
+		// ユーザーに対して入力値の制約を表示する
 		System.out.print("実数値：");
-		// 入力値をdouble型で宣言
+		// 入力値を仕様である大きいほうの値かどうかを比較するための変数として代入
 		double inputSecond = stdIn.nextDouble();
 		// 最初の入力値が大きければ変数maxに代入し、そうでなければ次の入力値を変数maxに代入する。
 		double max = inputFirst > inputSecond ? inputFirst : inputSecond;
