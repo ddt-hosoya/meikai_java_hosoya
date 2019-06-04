c45dae9a670b468ef492900a172a7bcdaecd9a4b
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-04 10:10:14 +0900

    2019/06/04_演習3-13_2回目_進捗率100％_コメントを修正

--- meikai_java/src/e03/e03_13.java
+++ meikai_java/src/e03/e03_13.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 
 public class e03_13 {
 	public static void main(String[] args){
-		// Scannerクラスの標準入力ストリームを実体化する
+		// 入力機能を実体化する
 		Scanner stdIn = new Scanner(System.in);
-		// 整数値：と表示する
+		// ユーザーに対して入力の制約を表示する
 		System.out.print("整数値：");
-		// 入力値をint型で宣言する
+		// 入力値を中央値を検査するための変数に代入
 		int inputFirst = stdIn.nextInt();
-		// 整数値：と表示する
+		// ユーザーに対して入力の制約を表示する
 		System.out.print("整数値：");
-		// 入力値をint型で宣言する
+		// 入力値を中央値を検査するための変数に代入
 		int inputSecond = stdIn.nextInt();
-		// 整数値：と表示する
+		// ユーザーに対して入力の制約を表示する
 		System.out.print("整数値：");
-		// 入力値をint型で宣言する
+		// 入力値を中央値を検査するための変数に代入
 		int inputThird = stdIn.nextInt();
 
-		// minを最初の入力値で初期化する
+		// 小さいほうの値を入れる変数minを最初の入力値で初期化する
 		int min = inputFirst;
-		// maxを最初の入力値で初期化する
+		// 大きいほうの値を入れる変数maxを最初の入力値で初期化する
 		int max = inputFirst;
 
-		// medianを三つ目の入力値で初期化する
+		// 中央値を入れる変数medianを三つ目の入力値で初期化する
 		int median = inputThird;
 
 		// 二つ目の入力値がminより小さい場合minに値を代入する
