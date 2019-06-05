aa9de1376265c35ef818278f78e8051d508c8817
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-05 17:19:04 +0900

    2019/06/05_演習4-12_2回目_進捗率100％_冗長したコードを修正

--- meikai_java/src/e04/e04_12.java
+++ meikai_java/src/e04/e04_12.java
@@ -11,22 +11,17 @@ public class e04_12 {
 		// カウントアップ開始の文言を表示
 		System.out.println("カウントアップします。");
 
-		// ユーザーに対して正の整数の入力を促す
-		System.out.print("正の整数：");
-		// 入力値を変数に代入する
-		int inputInt = stdIn.nextInt();
-
 		// カウントアップの最後の数いれる変数を宣言する
 		int endNumber;
 		// 入力値が0以下である場合入力を促す文言を繰り返し表示する
 		// 入力した値をカウントアップの最後の数いれる変数に代入する
-		for(endNumber = inputInt; endNumber <= 0; endNumber = stdIn.nextInt()){
+		for(endNumber = 0; endNumber <= 0; endNumber = stdIn.nextInt()){
 			// ユーザーに対して正の整数の入力を促す
 			System.out.print("正の整数：");
 		}
 
-		// カウントアップを0から開始するようcountupを0で初期化し、endNumberの値まで繰り返し処理を行う
-		// 表示処理実行後にcountupをインクリメントする
+		// カウントアップを0から開始し、endNumberの値まで繰り返し処理を行う
+		// 表示処理実行後にカウントアップする
 		for(int countup = 0; countup <= endNumber; countup++){
 			// 数を表示する
 			System.out.println(countup);
