1c6e437cd4709a633cb679b6a1a89d5f9c0c8a05
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-05 16:39:24 +0900

    2019/06/05_演習4-11_2回目_進捗率100％_do-while文をfor文に修正

--- meikai_java/src/e04/e04_11.java
+++ meikai_java/src/e04/e04_11.java
@@ -10,15 +10,21 @@ public class e04_11 {
 		Scanner stdIn = new Scanner(System.in);
 		// カウントダウン開始の文言を表示
 		System.out.println("カウントダウンします。");
+
 		// カウントダウン開始の数とする変数を宣言する
 		int startNumber;
-		do{
+
+		// ユーザーに対して正の整数値の入力を促す
+		System.out.print("正の整数値：");
+		// 入力値をカウントダウン開始の数とする変数に代入する
+		int inputInt = stdIn.nextInt();
+
+		// カウントダウン開始する数をいれる変数に入力値を代入し、その数が0以下の場合再度入力を促す処理を繰り返す
+		// 入力を促す処理を実行後に入力値をカウントダウン開始する数をいれる変数に入力値を代入
+		for(startNumber = inputInt; startNumber <= 0; startNumber = stdIn.nextInt()){
 			// ユーザーに対して正の整数値の入力を促す
-			System.out.print("正の整数値：");
-			// 入力値をカウントダウン開始の数とする変数に代入する
-			startNumber = stdIn.nextInt();
-		// 入力値が0以下の場合処理を繰り返す
-		}while(startNumber <= 0);
+			System.out.print("正の整数値：");			
+		}
 		// 表示する値を入れる変数countdownを入力値で初期化し、countdownが0になるまで処理を繰り返す。
 		// 処理実行後にcountdownをデクリメントする。
 		for(int countdown = startNumber; countdown >= 0; countdown--){
