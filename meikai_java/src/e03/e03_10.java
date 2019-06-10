e36e7050ba3b1629bb9f2231cc7dfb8553989430
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-10 20:18:12 +0900

    2019/06/10_演習3-10_3回目_進捗率100％_実数値を整数値に修正

--- meikai_java/src/e03/e03_10.java
+++ meikai_java/src/e03/e03_10.java
@@ -1,6 +1,6 @@
 package e03;
 /*
-二つの実数値を読み込んで、それらの値の差を表示するプログラムを作成せよ。
+二つの整数値を読み込んで、それらの値の差を表示するプログラムを作成せよ。
  */
 import java.util.Scanner;
 
@@ -9,15 +9,15 @@ public class e03_10 {
 		// 入力機能を実体化する
 		Scanner stdIn = new Scanner(System.in);
 		// ユーザーに対して入力値の制約を表示する
-		System.out.print("実数値：");
+		System.out.print("整数値：");
 		// 入力値の差を求めるための変数に代入
-		double inputFirst = stdIn.nextDouble();
+		int inputFirst = stdIn.nextInt();
 		// ユーザーに対して入力値の制約を表示する
-		System.out.print("実数値：");
+		System.out.print("整数値：");
 		// 入力値の差を求めるための変数に代入
-		double inputSecond = stdIn.nextDouble();
+		int inputSecond = stdIn.nextInt();
 		// 大きいほうの値から小さいほうの値を引いた差を変数に代入する
-		double difference = inputFirst > inputSecond ? (inputFirst - inputSecond) : (inputSecond - inputFirst);
+		int difference = inputFirst > inputSecond ? (inputFirst - inputSecond) : (inputSecond - inputFirst);
 		// 二つの値の差を表示する
 		System.out.println("二つの値の差は" + difference + "です。");
 	}
