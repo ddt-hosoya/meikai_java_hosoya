9cb3a64efe990c817df8c77d48656b5a6630866a
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-12 18:10:31 +0900

    2019/06/12_演習7-5_1回目_進捗率100％

---
+++ meikai_java/src/e07/e07_05.java
@@ -0,0 +1,17 @@
+package e07;
+/*
+『こんにちは。』と表示するメソッドhelloを作成せよ。
+void hello()
+ */
+public class e07_05 {
+	//『こんにちは。』と表示するメソッド
+	static void hello(){
+		System.out.println("こんにちは。");
+		return;
+	}
+	// mainメソッド
+	public static void main(String[] arg){
+		hello();
+	}
+
+}
