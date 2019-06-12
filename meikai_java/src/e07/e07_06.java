814aff16816f2c33e9027d79404f8634df9c2fd7
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-12 19:02:00 +0900

    2019/06/12_演習7-6_1回目_進捗率100％

---
+++ meikai_java/src/e07/e07_06.java
@@ -0,0 +1,33 @@
+package e07;
+/*
+引数mで指定された月の季節を表示するメソッドprintSeasonを作成せよ。
+mが3, 4, 5であれば「春」、6, 7, 8であれば「夏」、9, 10, 11 であれば「秋」、
+12, 1, 2であれば「冬」と表示し、それ以外であれば何も表示しないこと。
+ */
+import java.util.Scanner;
+
+public class e07_06 {
+	// 季節を表示するメソッド
+	static void printSeason(int n){
+		// 3～5が入力された場合、春と表示
+		if(n >= 3 && n <= 5){System.out.println("春");}
+		// 6～8が入力された場合、夏と表示
+		if(n >= 6 && n <= 8){System.out.println("夏");}
+		// 9～11が入力された場合、秋と表示
+		if(n >= 9 && n <= 11){System.out.println("秋");}
+		// 12または1または2が入力された場合、冬と表示
+		if(n == 12 || n == 1 || n == 2){System.out.println("冬");}
+	}
+	// mainメソッド
+	public static void main(String[] args){
+		// 入力機能を実体化
+		Scanner stdIn = new Scanner(System.in);
+		// 月の入力を促す
+		System.out.print("月：");
+		// 入力値を季節を判定するための月にいれる
+		int n = stdIn.nextInt();
+		// 季節を表示する
+		printSeason(n);
+	}
+
+}
