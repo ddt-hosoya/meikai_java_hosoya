52d62d9a72bfa52e7d9f6b12a397575839ebbdff
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-18 13:32:23 +0900

    2019/06/18_演習7-27_1回目_進捗率100％

---
+++ meikai_java/src/e07/e07_27.java
@@ -0,0 +1,67 @@
+package e07;
+/*
+List7-20のプログラムは、三つの配列x,y,zの行数および列数が同一であることを前提としている。
+三つの配列の要素が等しければ加算を行ってtrueを返し、等しくなければ加算を行わずにfalseを返すメソッドに書き換えよ。
+ */
+
+public class e07_27 {
+	// 行列xとyの和をzに代入
+	static boolean addMatrix(int[][] x, int[][] y, int[][] z){
+		// 三つの配列の行数が異なる場合
+		if(x.length != y.length || x.length != z.length){
+			// falseを返す
+			return false;
+		}
+		// 行ごとに要素の加算処理をおこなう
+		for(int i = 0; i < x.length; i++){
+			// 三つの配列の列数がことなる場合
+			if(x[i].length != y[i].length || x[i].length != z[i].length){
+				// falseを返す
+				return false;
+			}
+			// 各列ごとに加算処理を行い結果を配列zに代入
+			for(int j = 0; j < x[i].length; j++){
+				// 加算処理を行い結果を配列zに代入
+				z[i][j] = x[i][j] + y[i][j];
+			}
+		}
+		// trueを返す
+		return true;
+	}
+	// 行列mの全要素を表示
+	static void printMatrix(int[][] m){
+		// 全行数分繰り返す
+		for(int i = 0; i < m.length; i++){
+			// 各行の前列数分繰り返す
+			for(int j = 0; j < m[i].length; j++){
+				// 要素の値を表示
+				System.out.print(m[i][j] + " ");
+			}
+			// 行ごとに改行
+			System.out.println();
+		}
+	}
+	// mainメソッド
+	public static void main(String[] args){
+		// 配列a
+		int[][] a = {{1,2,3},{4,5,6}};
+		// 配列b
+		int[][] b = {{6,3,4},{5,1,2}};
+		// 配列c
+		int[][] c = new int[3][3];
+
+		// 行列aとbの和をcに代入
+		boolean result = addMatrix(a, b, c);
+
+		// 行列aの全要素を表示
+		System.out.println("行列a"); printMatrix(a);
+		// 行列bの全要素を表示
+		System.out.println("行列b"); printMatrix(b);
+		// 加算処理が完了していればcの全要素を表示
+		if(result){
+			// 行列cの全要素を表示
+			System.out.println("行列c"); printMatrix(c);
+		}
+	}
+
+}
