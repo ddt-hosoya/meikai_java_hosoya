a20befc289deb13c15105c1087a6df95928df26b
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-24 18:25:10 +0900

    2019/06/24_演習10-2_1回目_進捗率100％_連番Exクラス

---
+++ meikai_java/src/e10/e10_02_ExId.java
@@ -0,0 +1,41 @@
+package e10;
+/*
+前問で作成したクラスIdを、以下のように変更したクラスExIdを作成せよ。
+インスタンスを生成するたびに識別番号をnずつ増やして与える(nは正の整数)。nの値は、
+指定されない限り1とするが、メソッドを通じて取得、変更できるようにする。
+ */
+public class e10_02_ExId {
+	// 何番までの識別番号を与えたか
+	private static int counter = 0;
+	// 識別番号の増加間隔
+	private static int n = 1;
+	// 識別番号
+	private int id;
+
+
+	// 最後に与えた識別番号を返すクラスメソッド
+	public static int getMaxId(){
+		// 最後に与えた識別番号を返す
+		return counter;
+	}
+	// 識別番号の増加間隔を返すクラスメソッド
+	public static int getN(){
+		return n;
+	}
+	// 識別番号の増加間隔を変更するクラスメソッド
+	public static void setN(int changeN){
+		n = changeN;
+	}
+
+	// コンストラクタ
+	public e10_02_ExId(){
+		counter += n;
+		// 何番までの識別番号を与えたかの値をインクリメントして識別番号にいれる
+		id = counter;
+	}
+	// そのインスタンスの識別番号を返すインスタンスメソッド
+	public int getId(){
+		return id;
+	}
+
+}
