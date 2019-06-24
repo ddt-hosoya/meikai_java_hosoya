80ca31d22c5f1fbbc6d9e619684b4ca8cd5f56fd
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-24 10:09:35 +0900

    2019/06/24_演習9-3_3回目_進捗率100％_テスト用

---
+++ meikai_java/src/e09/e09_03_tester.java
@@ -0,0 +1,20 @@
+package e09;
+
+import java.util.Scanner;
+
+public class e09_03_tester {
+	public static void main(String[] args){
+		Scanner stdIn = new Scanner(System.in);
+
+		e09_03_Day adachiOpenDay = new e09_03_Day(2019, 6, 21);
+
+		e09_03 adachi = new e09_03("足立幸一", "123456", 1000, adachiOpenDay);
+
+		System.out.println("■足立君の口座");
+		System.out.println("　口座名義；" + adachi.getName());
+		System.out.println("　口座番号；" + adachi.getNo());
+		System.out.println("　貯金残高；" + adachi.getBalance());
+		System.out.println("　口座開設日；" + adachi.getOpenDay());
+	}
+
+}
