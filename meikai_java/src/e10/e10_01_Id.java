353d4c75c468b030b1c5db0c4bc5b9e18b06e807
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-06-24 17:47:11 +0900

    2019/06/24_演習10-1_1回目_進捗率100％_連番クラス

---
+++ meikai_java/src/e10/e10_01_Id.java
@@ -0,0 +1,29 @@
+package e10;
+/*
+List10-3に示した連番クラスIdに、最後に与えた識別番号を返すクラスメソッドgetMaxIdを追加せよ。
+なお、このメソッドはインスタンスをn個生成した時点で呼び出すとnを返すことになる。
+ */
+public class e10_01_Id {
+	// 何番までの識別番号を与えたか
+	private static int counter = 0;
+	// 識別番号
+	private int id;
+
+	// 最後に与えた識別番号を返すクラスメソッド
+	public static int getMaxId(){
+		// 最後に与えた識別番号を返す
+		return counter;
+	}
+
+	// コンストラクタ
+	public e10_01_Id(){
+		// 何番までの識別番号を与えたかの値をインクリメントして識別番号にいれる
+		id = ++ counter;
+	}
+	// そのインスタンスの識別番号を返すインスタンスメソッド
+	public int getId(){
+		return id;
+	}
+}
+
+
