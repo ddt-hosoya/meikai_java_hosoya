f780978e4a925ee2cefd9f3502d22923480fe32c
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-07-01 20:55:32 +0900

    2019/07/01_演習13-1_1回目_進捗率100％_Point

---
+++ meikai_java/src/e13/e13_01_Point.java
@@ -0,0 +1,24 @@
+package e13;
+/*
+クラスPointは、点を表すクラスです
+このクラスは図形を表す抽象クラスShapeから派生したクラスです
+ */
+public class e13_01_Point extends e13_01_Shape {
+	// 点を生成するコンストラクタです
+	public e13_01_Point(){
+		//何も行わない
+	}
+
+	// メソッドtoStrongは、点に関する図形情報を表す文字列を返却します
+	public String toString(){
+		// "Point"という文字列を返す
+		return "Point";
+	}
+
+	// メソッドdrawは点を描画するメソッドです
+	public void draw(){
+		// プラス記号を一個だけ表示して改行
+		System.out.println('+');
+	}
+
+}
