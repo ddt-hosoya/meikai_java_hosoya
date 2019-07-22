930c91b6aa74fdb1fa10a1d7f32951aaf34a3b4f
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-07-22 20:37:08 +0900

    2019/07/22_演習7-2_2回目_進捗率100％_メソッドコメント追加_を追加修正

--- meikai_java/src/e07/e07_02.java
+++ meikai_java/src/e07/e07_02.java
@@ -6,10 +6,12 @@ int min(int a, int b, int c)
 import java.util.Scanner;
 
 public class e07_02 {
-	/*
+	/**
 	 *  三つのint型引数a,b,cの最小値を求めるメソッド
+	 *
 	 *  @author t.hosoya
 	 *  @since 20190612
+	 *
 	 */
 	static int min(int a, int b, int c){
 		// aを仮に最小値とする
