79b2e38dace4064992753f29e246bdaff73792e3
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-10-02 20:03:37 +0900

    2019/10/02_演習7-14_3回目_進捗率100％_変数宣言及び反転のロジックを修正、冗長処理のメソッド化

--- meikai_java/src/e07/e07_14.java
+++ meikai_java/src/e07/e07_14.java
@@ -6,11 +6,6 @@ package e07;
 import java.util.Scanner;
 
 public class e07_14 {
-	// posビット目を1,0,反転した値にするための調整値
-	static int adjustZero = 0;
-	static int adjustOne = 1;
-	static int adjust = 0;
-
 	/**
 	 *  整数のビット表示を行うメソッド
 	 * @author t.hosoya
@@ -27,6 +22,28 @@ public class e07_14 {
 		System.out.println();
 	}
 	/**
+	 * 0をposビット目を最下位として連続するn個のビットを、1にした値を返すメソッド
+	 * @author t.hosoya
+	 * @since 20191002
+	 * @param pos ビットを1にする最下位のビット目
+	 * @param n 1にするビットの個数
+	 * @return int 0をposビット目を最下位として連続するn個のビットを、1にした値
+	 */
+	static int setFlagContinue(int pos, int n){
+		// xの対象のビット位置に1をたてるための値
+		int flag = 0;
+		// posビット目から左にn個分を連続する1にする
+		for(int index=0; index < n; index++){
+			// どのビット位置に1をたてるかを示す値
+			int setFlag = 1;
+			// posビット目から左にn個目を1にする
+			setFlag = setFlag << (pos + index);
+			// posビット目から左にn個目を1にした値を増やしていく
+			flag = flag | setFlag;
+		}
+		return flag;
+	}
+	/**
 	 *  整数xのposビット目を最下位として連続するn個のビットを、1にした値を返すメソッド
 	 * @author t.hosoya
 	 * @since 20190617
@@ -36,22 +53,18 @@ public class e07_14 {
 	 * @return int 編集後の整数
 	 */
 	static int setN(int x, int pos, int n){
-		// posビット目から左にn個分を連続する1にした値を調整値とする
-		for(int index=0; index < n; index++){
-			// 1を立てるための調整値
-			int base = 1;
-			// posビット目から左にn個目を1にする
-			base = base << (pos + index);
-			// posビット目から左にn個目を1にした値を増やしていく
-			adjust = adjust | base;
-		}
+		// xの対象のビット位置に1をたてるための値
+		int flag = setFlagContinue(pos, n);
+
 		// 入力した整数のビットの値と合わせてどちらかが1であればそのビット位置を1にする
-		int setResult = x | adjust;
+		int setResult = x | flag;
 
 		// 処理の内容を表示
 		System.out.println(pos + "ビット目から"+ n +"個を1にしました。");
+
 		// ビット内容の表示
 		putBits(setResult);
+
 		// posビット目を1にした値を返す
 		return setResult;
 	}
@@ -66,24 +79,21 @@ public class e07_14 {
 	 * @return int 編集後の整数
 	 */
 	static int resetN(int x, int pos, int n){
-		// posビット目から左にn個分を連続する1にした値を調整値とする
-		for(int index=0; index < n; index++){
-			// 1を立てるための調整値
-			int base = 1;
-			// posビット目から左にn個目を1にする
-			base = base << (pos + index);
-			// posビット目から左にn個目を1にした値を増やしていく
-			adjust = adjust | base;
-		}
+		// xの対象のビット位置に0をたてるための値
+		int flag = setFlagContinue(pos, n);
+
 		// posビット目から左にn個分を連続する0、それ以外は1にした値を調整値とする
-		adjust = ~adjust;
+		flag = ~flag;
+
 		// 入力した整数のビットの値と合わせてどちらも1であればそのビット位置を1にする
-		int resetResult = x & adjust;
+		int resetResult = x & flag;
 
 		// 処理の内容を表示
 		System.out.println(pos + "ビット目から"+ n +"個を0にしました。");
+
 		// ビット内容の表示
 		putBits(resetResult);
+
 		// posビット目から左にn個分を連続する0にした値を返す
 		return resetResult;
 	}
@@ -98,46 +108,18 @@ public class e07_14 {
 	 * @return int 編集後の整数
 	 */
 	static int inverseN(int x, int pos, int n){
-		// 調整値の値を初期化する
-		adjust = 0;
-		// posビット目を最下位として連続するn個分の値を調査する
-		for(int index=0; index < n; index++){
-			// 1を立てるための調整値
-			int base = 1;
-			// 整数のそのビット位置が1の場合posビット目を0に変更する
-			if((x >>> (pos + index) & adjustOne) == adjustOne){
-				// posビット目から左にn個目を1にする
-				base = base << (pos + index);
-				// posビット目から左にn個目を1にした値を増やしていく
-				adjust = adjust | base;
-			}
-		}
-		// 1の立っているビット位置に1を立てた調整値を反転させる
-		adjust = ~adjust;
-		// 整数のposビット目を最下位として連続するn個のビットのなかで1になっている個所を0にする
-		int inverseResult = x & adjust;
+		// xの対象のビット位置の値を反転させるための値
+		int flag = setFlagContinue(pos, n);
 
-		// 調整値の値を初期化する
-		adjust = 0;
-		// posビット目を最下位として連続するn個分の値を調査する
-		for(int index=0; index < n; index++){
-			// 1を立てるための調整値
-			int base = 1;
-		// 整数のposビット目が0の場合posビット目を1に変更する
-			if((x >>> (pos + index) & adjustOne) != adjustOne){
-				// posビット目から左にn個目を1にする
-				base = base << (pos + index);
-				// posビット目から左にn個目を1にした値を増やしていく
-				adjust = adjust | base;
-			}
-		}
-		// 整数のposビット目を最下位として連続するn個のビットのなかで0になっている個所を1にする
-		inverseResult = inverseResult | adjust;
+		// 整数のposビット目を最下位として連続するn個のビットのなかで1になっている個所を0にし、0になっている個所を1にする
+		int inverseResult = x ^ flag;
 
 		// 処理の内容を表示
 		System.out.println(pos + "ビット目から"+ n +"個を反転しました。");
+
 		// ビット内容の表示
 		putBits(inverseResult);
+
 		// 変更した値を返す
 		return inverseResult;
 	}
