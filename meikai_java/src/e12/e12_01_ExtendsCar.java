e6222f8e6f9e3f3c68371f6de6c4dd4643a772ba
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-10-20 15:53:43 +0900

    2019/10/20_演習12-1_2回目_進捗率100％_クラス・メソッドコメント追加

--- meikai_java/src/e12/e12_01_ExtendsCar.java
+++ meikai_java/src/e12/e12_01_ExtendsCar.java
@@ -3,6 +3,12 @@ package e12;
 総走行距離を表すフィールドと、その値を調べるメソッドを追加した自動車クラスを作成せよ。
 自動車クラスCar第二版(p.324)から派生すること。
  */
+/**
+ * 自動車クラス拡張版
+ * @author t.hosoya
+ * @since 20190627
+ *
+ */
 public class e12_01_ExtendsCar extends e12_01_Car{
 	private double distance;
 
@@ -11,12 +17,24 @@ public class e12_01_ExtendsCar extends e12_01_Car{
 		super(name, width, height, length, fuel, purchaseDay);
 		this.distance = 0;
 	}
-	// 総走行距離を取得
+	/**
+	 * 総走行距離を取得
+	 * @author t.hosoya
+	 * @since 20190627
+	 * @return double 総走行距離
+	 */
 	double getDistance(){
 		// 総走行距離を返す
 		return distance;
 	}
-	// スーパークラスのメソッドをオーバーライド
+	/**
+	 * スーパークラスのメソッドをオーバーライド
+	 * @author t.hosoya
+	 * @since 20190627
+	 * @param dx x方向に進む距離
+	 * @param dy y方向に進む距離
+	 * @retun boolean 移動できたかどうか
+	 */
 	public boolean move(double dx, double dy){
 		// 移動できたかどうか
 		boolean result = super.move(dx, dy);
