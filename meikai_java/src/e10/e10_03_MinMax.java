585112adbf3a148193c3e898c518635f078ee9dc
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2019-10-20 14:03:21 +0900

    2019/10/20_演習10-3_3回目_進捗率100％_クラス・メソッドコメント追加

--- meikai_java/src/e10/e10_03_MinMax.java
+++ meikai_java/src/e10/e10_03_MinMax.java
@@ -2,9 +2,21 @@ package e10;
 /*
 二値/三値/配列の最小値や最大値を求めるメソッドを集めたユーティリティクラスMinMaxを作成せよ。
  */
+/**
+ * 二値/三値/配列の最小値や最大値を求めるメソッドを集めたユーティリティクラス
+ * @author t.hosoya
+ * @since 20190624
+ *
+ */
 public final class e10_03_MinMax {
-	// 二値最小値
-	// 二値の最小値を求める(int型)
+	/**
+	 * 二値の最小値を求める(int型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a int型整数値a
+	 * @param b int型整数値b
+	 * @return int 二値の最小値
+	 */
 	public static int min(int a, int b){
 		// aを仮に最小値とする
 		int min = a;
@@ -13,7 +25,14 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 二値の最小値を求める(long型)
+	/**
+	 * 二値の最小値を求める(long型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a long型整数値a
+	 * @param b long型整数値b
+	 * @return long 二値の最小値
+	 */
 	public static long min(long a, long b){
 		// aを仮に最小値とする
 		long min = a;
@@ -22,7 +41,14 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 二値の最小値を求める(float型)
+	/**
+	 * 二値の最小値を求める(float型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a float型整数値a
+	 * @param b float型整数値b
+	 * @return float 二値の最小値
+	 */
 	public static float min(float a, float b){
 		// aを仮に最小値とする
 		float min = a;
@@ -31,7 +57,14 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 二値の最小値を求める(double型)
+	/**
+	 * 二値の最小値を求める(double型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a double型整数値a
+	 * @param b double型整数値b
+	 * @return double 二値の最小値
+	 */
 	public static double min(double a, double b){
 		// aを仮に最小値とする
 		double min = a;
@@ -40,8 +73,15 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 三値最小値
-	// 三値の最小値を求める(int型)
+	/**
+	 * 三値の最小値を求める(int型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a int型整数値a
+	 * @param b int型整数値b
+	 * @param c int型整数値c
+	 * @return int 三値の最小値
+	 */
 	public static int min(int a, int b, int c){
 		// 二値の最小値を求める
 		int min = min(a, b);
@@ -50,7 +90,15 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 三値の最小値を求める(long型)
+	/**
+	 * 三値の最小値を求める(long型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a long型整数値a
+	 * @param b long型整数値b
+	 * @param c long型整数値c
+	 * @return long 三値の最小値
+	 */
 	public static long min(long a, long b, long c){
 		// 二値の最小値を求める
 		long min = min(a, b);
@@ -59,7 +107,15 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 三値の最小値を求める(float型)
+	/**
+	 * 三値の最小値を求める(float型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a float型整数値a
+	 * @param b float型整数値b
+	 * @param c float型整数値c
+	 * @return float 三値の最小値
+	 */
 	public static float min(float a, float b, float c){
 		// 二値の最小値を求める
 		float min = min(a, b);
@@ -68,7 +124,15 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 三値の最小値を求める(double型)
+	/**
+	 * 三値の最小値を求める(double型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a double型整数値a
+	 * @param b double型整数値b
+	 * @param c double型整数値c
+	 * @return double 三値の最小値
+	 */
 	public static double min(double a, double b, double c){
 		// 二値の最小値を求める
 		double min = min(a, b);
@@ -78,8 +142,13 @@ public final class e10_03_MinMax {
 		return min;
 	}
 
-	// 配列最小値
-	// 配列の最小値を求める(int型)
+	/**
+	 * 配列の最小値を求める(int型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array int型整数値を要素として持つ配列
+	 * @return int 配列の最小値
+	 */
 	public static int min(int[] array){
 		// 配列の先頭の値を仮に最小値とする
 		int min = array[0];
@@ -91,7 +160,13 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 配列の最小値を求める(long型)
+	/**
+	 * 配列の最小値を求める(long型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array long型整数値を要素として持つ配列
+	 * @return long 配列の最小値
+	 */
 	public static long min(long[] array){
 		// 配列の先頭の値を仮に最小値とする
 		long min = array[0];
@@ -103,7 +178,13 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 配列の最小値を求める(float型)
+	/**
+	 * 配列の最小値を求める(float型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array float型整数値を要素として持つ配列
+	 * @return float 配列の最小値
+	 */
 	public static float min(float[] array){
 		// 配列の先頭の値を仮に最小値とする
 		float min = array[0];
@@ -114,7 +195,14 @@ public final class e10_03_MinMax {
 		}
 		// 最小値を返す
 		return min;
-	}// 配列の最小値を求める(double型)
+	}
+	/**
+	 * 配列の最小値を求める(double型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array double型整数値を要素として持つ配列
+	 * @return double 配列の最小値
+	 */
 	public static double min(double[] array){
 		// 配列の先頭の値を仮に最小値とする
 		double min = array[0];
@@ -126,8 +214,14 @@ public final class e10_03_MinMax {
 		// 最小値を返す
 		return min;
 	}
-	// 二値最大値
-	// 二値の最大値を求める(int型)
+	/**
+	 * 二値の最大値を求める(int型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a int型整数値a
+	 * @param b int型整数値b
+	 * @return int 二値の最小値
+	 */
 	public static int max(int a, int b){
 		// 最大値を仮にaとする
 		int max = a;
@@ -136,7 +230,14 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 二値の最大値を求める(long型)
+	/**
+	 * 二値の最大値を求める(long型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a long型整数値a
+	 * @param b long型整数値b
+	 * @return long 二値の最小値
+	 */
 	public static long max(long a, long b){
 		// 最大値を仮にaとする
 		long max = a;
@@ -145,7 +246,14 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 二値の最大値を求める(float型)
+	/**
+	 * 二値の最大値を求める(float型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a float型整数値a
+	 * @param b float型整数値b
+	 * @return float 二値の最小値
+	 */
 	public static float max(float a, float b){
 		// 最大値を仮にaとする
 		float max = a;
@@ -154,7 +262,14 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 二値の最大値を求める(double型)
+	/**
+	 * 二値の最大値を求める(double型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a double型整数値a
+	 * @param b double型整数値b
+	 * @return double 二値の最小値
+	 */
 	public static double max(double a, double b){
 		// 最大値を仮にaとする
 		double max = a;
@@ -163,8 +278,15 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 三値最大値
-	// 三値の最大値を求める(int型)
+	/**
+	 * 三値の最大値を求める(int型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a int型整数値a
+	 * @param b int型整数値b
+	 * @param c int型整数値c
+	 * @return int 三値の最小値
+	 */
 	public static int max(int a, int b, int c){
 		// 二値の最大値を求める
 		int max = max(a, b);
@@ -173,7 +295,15 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 三値の最大値を求める(long型)
+	/**
+	 * 三値の最大値を求める(long型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a long型整数値a
+	 * @param b long型整数値b
+	 * @param c long型整数値c
+	 * @return long 三値の最小値
+	 */
 	public static long max(long a, long b, long c){
 		// 二値の最大値を求める
 		long max = max(a, b);
@@ -182,7 +312,15 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 三値の最大値を求める(float型)
+	/**
+	 * 三値の最大値を求める(float型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a float型整数値a
+	 * @param b float型整数値b
+	 * @param c float型整数値c
+	 * @return float 三値の最小値
+	 */
 	public static float max(float a, float b, float c){
 		// 二値の最大値を求める
 		float max = max(a, b);
@@ -191,7 +329,15 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 三値の最大値を求める(double型)
+	/**
+	 * 三値の最大値を求める(double型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param a double型整数値a
+	 * @param b double型整数値b
+	 * @param c double型整数値c
+	 * @return double 三値の最小値
+	 */
 	public static double max(double a, double b, double c){
 		// 二値の最大値を求める
 		double max = max(a, b);
@@ -200,8 +346,13 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 配列最大値
-	// 配列の最大値を求めるメソッド(int型)
+	/**
+	 * 配列の最大値を求める(int型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array int型整数値を要素として持つ配列
+	 * @return int 配列の最小値
+	 */
 	public static int max(int[] array){
 		// 配列の先頭の値を仮に最大値とする
 		int max = array[0];
@@ -213,7 +364,13 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 配列の最大値を求めるメソッド(long型)
+	/**
+	 * 配列の最大値を求める(long型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array long型整数値を要素として持つ配列
+	 * @return long 配列の最小値
+	 */
 	public static long max(long[] array){
 		// 配列の先頭の値を仮に最大値とする
 		long max = array[0];
@@ -225,7 +382,13 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 配列の最大値を求めるメソッド(float型)
+	/**
+	 * 配列の最大値を求める(float型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array float型整数値を要素として持つ配列
+	 * @return float 配列の最小値
+	 */
 	public static float max(float[] array){
 		// 配列の先頭の値を仮に最大値とする
 		float max = array[0];
@@ -237,7 +400,13 @@ public final class e10_03_MinMax {
 		// 最大値を返す
 		return max;
 	}
-	// 配列の最大値を求めるメソッド(double型)
+	/**
+	 * 配列の最大値を求める(double型)
+	 * @author t.hosoya
+	 * @since 20190624
+	 * @param array double型整数値を要素として持つ配列
+	 * @return double 配列の最小値
+	 */
 	public static double max(double[] array){
 		// 配列の先頭の値を仮に最大値とする
 		double max = array[0];
