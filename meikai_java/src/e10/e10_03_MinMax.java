78e71d601aa67d31174d02e60bb8d9e29e24b71e
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2020-07-15 21:02:22 +0900

    2020/07/15_演習10-3_4回目_進捗率100％_変数名修正

--- meikai_java/src/e10/e10_03_MinMax.java
+++ meikai_java/src/e10/e10_03_MinMax.java
@@ -13,15 +13,15 @@ public final class e10_03_MinMax {
 	 * 二値の最小値を求める(int型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a int型整数値a
-	 * @param b int型整数値b
+	 * @param firstNumber int型整数値firstNumber
+	 * @param secondNumber int型整数値secondNumber
 	 * @return int 二値の最小値
 	 */
-	public static int min(int a, int b){
+	public static int min(int firstNumber, int secondNumber){
 		// aを仮に最小値とする
-		int min = a;
-		// 最小値よりbが小さい場合、bを最小値とする
-		if(min > b){ min = b; }
+		int min = firstNumber;
+		// 最小値よりsecondNumberが小さい場合、secondNumberを最小値とする
+		if(min > secondNumber){ min = secondNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -29,15 +29,15 @@ public final class e10_03_MinMax {
 	 * 二値の最小値を求める(long型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a long型整数値a
-	 * @param b long型整数値b
+	 * @param firstNumber long型整数値firstNumber
+	 * @param secondNumber long型整数値secondNumber
 	 * @return long 二値の最小値
 	 */
-	public static long min(long a, long b){
-		// aを仮に最小値とする
-		long min = a;
-		// 最小値よりbが小さい場合、bを最小値とする
-		if(min > b){ min = b; }
+	public static long min(long firstNumber, long secondNumber){
+		// firstNumberを仮に最小値とする
+		long min = firstNumber;
+		// 最小値よりsecondNumberが小さい場合、secondNumberを最小値とする
+		if(min > secondNumber){ min = secondNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -45,15 +45,15 @@ public final class e10_03_MinMax {
 	 * 二値の最小値を求める(float型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a float型整数値a
-	 * @param b float型整数値b
+	 * @param firstNumber float型整数値firstNumber
+	 * @param secondNumber float型整数値secondNumber
 	 * @return float 二値の最小値
 	 */
-	public static float min(float a, float b){
-		// aを仮に最小値とする
-		float min = a;
-		// 最小値よりbが小さい場合、bを最小値とする
-		if(min > b){ min = b; }
+	public static float min(float firstNumber, float secondNumber){
+		// firstNumberを仮に最小値とする
+		float min = firstNumber;
+		// 最小値よりsecondNumberが小さい場合、secondNumberを最小値とする
+		if(min > secondNumber){ min = secondNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -61,15 +61,15 @@ public final class e10_03_MinMax {
 	 * 二値の最小値を求める(double型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a double型整数値a
-	 * @param b double型整数値b
+	 * @param firstNumber double型整数値firstNumber
+	 * @param secondNumber double型整数値secondNumber
 	 * @return double 二値の最小値
 	 */
-	public static double min(double a, double b){
-		// aを仮に最小値とする
-		double min = a;
-		// 最小値よりbが小さい場合、bを最小値とする
-		if(min > b){ min = b; }
+	public static double min(double firstNumber, double secondNumber){
+		// firstNumberを仮に最小値とする
+		double min = firstNumber;
+		// 最小値よりsecondNumberが小さい場合、secondNumberを最小値とする
+		if(min > secondNumber){ min = secondNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -77,16 +77,16 @@ public final class e10_03_MinMax {
 	 * 三値の最小値を求める(int型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a int型整数値a
-	 * @param b int型整数値b
-	 * @param c int型整数値c
+	 * @param firstNumber int型整数値firstNumber
+	 * @param secondNumber int型整数値secondNumber
+	 * @param thirdNumber int型整数値thirdNumber
 	 * @return int 三値の最小値
 	 */
-	public static int min(int a, int b, int c){
+	public static int min(int firstNumber, int secondNumber, int thirdNumber){
 		// 二値の最小値を求める
-		int min = min(a, b);
-		// 最小値よりcが小さい場合、cを最小値とする
-		if(min > c){ min = c; }
+		int min = min(firstNumber, secondNumber);
+		// 最小値よりthirdNumberが小さい場合、thirdNumberを最小値とする
+		if(min > thirdNumber){ min = thirdNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -94,16 +94,16 @@ public final class e10_03_MinMax {
 	 * 三値の最小値を求める(long型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a long型整数値a
-	 * @param b long型整数値b
-	 * @param c long型整数値c
+	 * @param firstNumber long型整数値firstNumber
+	 * @param secondNumber long型整数値secondNumber
+	 * @param thirdNumber long型整数値thirdNumber
 	 * @return long 三値の最小値
 	 */
-	public static long min(long a, long b, long c){
+	public static long min(long firstNumber, long secondNumber, long thirdNumber){
 		// 二値の最小値を求める
-		long min = min(a, b);
-		// 最小値よりcが小さい場合、cを最小値とする
-		if(min > c){ min = c; }
+		long min = min(firstNumber, secondNumber);
+		// 最小値よりthirdNumberが小さい場合、thirdNumberを最小値とする
+		if(min > thirdNumber){ min = thirdNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -111,16 +111,16 @@ public final class e10_03_MinMax {
 	 * 三値の最小値を求める(float型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a float型整数値a
-	 * @param b float型整数値b
-	 * @param c float型整数値c
+	 * @param firstNumber float型整数値firstNumber
+	 * @param secondNumber float型整数値secondNumber
+	 * @param thirdNumber float型整数値thirdNumber
 	 * @return float 三値の最小値
 	 */
-	public static float min(float a, float b, float c){
+	public static float min(float firstNumber, float secondNumber, float thirdNumber){
 		// 二値の最小値を求める
-		float min = min(a, b);
-		// 最小値よりcが小さい場合、cを最小値とする
-		if(min > c){ min = c; }
+		float min = min(firstNumber, secondNumber);
+		// 最小値よりthirdNumberが小さい場合、thirdNumberを最小値とする
+		if(min > thirdNumber){ min = thirdNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -128,16 +128,16 @@ public final class e10_03_MinMax {
 	 * 三値の最小値を求める(double型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a double型整数値a
-	 * @param b double型整数値b
-	 * @param c double型整数値c
+	 * @param firstNumber double型整数値firstNumber
+	 * @param secondNumber double型整数値secondNumber
+	 * @param thirdNumber double型整数値thirdNumber
 	 * @return double 三値の最小値
 	 */
-	public static double min(double a, double b, double c){
+	public static double min(double firstNumber, double secondNumber, double thirdNumber){
 		// 二値の最小値を求める
-		double min = min(a, b);
-		// 最小値よりcが小さい場合、cを最小値とする
-		if(min > c){ min = c; }
+		double min = min(firstNumber, secondNumber);
+		// 最小値よりthirdNumberが小さい場合、thirdNumberを最小値とする
+		if(min > thirdNumber){ min = thirdNumber; }
 		// 最小値を返す
 		return min;
 	}
@@ -218,15 +218,15 @@ public final class e10_03_MinMax {
 	 * 二値の最大値を求める(int型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a int型整数値a
-	 * @param b int型整数値b
+	 * @param firstNumber int型整数値firstNumber
+	 * @param secondNumber int型整数値secondNumber
 	 * @return int 二値の最小値
 	 */
-	public static int max(int a, int b){
-		// 最大値を仮にaとする
-		int max = a;
-		// 最大値よりbが大きければ最大値をbとする
-		if(max < b){ max = b; }
+	public static int max(int firstNumber, int secondNumber){
+		// 最大値を仮にfirstNumberとする
+		int max = firstNumber;
+		// 最大値よりsecondNumberが大きければ最大値をsecondNumberとする
+		if(max < secondNumber){ max = secondNumber; }
 		// 最大値を返す
 		return max;
 	}
@@ -234,15 +234,15 @@ public final class e10_03_MinMax {
 	 * 二値の最大値を求める(long型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a long型整数値a
-	 * @param b long型整数値b
+	 * @param firstNumber long型整数値firstNumber
+	 * @param secondNumber long型整数値secondNumber
 	 * @return long 二値の最小値
 	 */
-	public static long max(long a, long b){
-		// 最大値を仮にaとする
-		long max = a;
-		// 最大値よりbが大きければ最大値をbとする
-		if(max < b){ max = b; }
+	public static long max(long firstNumber, long secondNumber){
+		// 最大値を仮にfirstNumberとする
+		long max = firstNumber;
+		// 最大値よりsecondNumberが大きければ最大値をsecondNumberとする
+		if(max < secondNumber){ max = secondNumber; }
 		// 最大値を返す
 		return max;
 	}
@@ -250,15 +250,15 @@ public final class e10_03_MinMax {
 	 * 二値の最大値を求める(float型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a float型整数値a
-	 * @param b float型整数値b
+	 * @param firstNumber float型整数値firstNumber
+	 * @param secondNumber float型整数値secondNumber
 	 * @return float 二値の最小値
 	 */
-	public static float max(float a, float b){
-		// 最大値を仮にaとする
-		float max = a;
-		// 最大値よりbが大きければ最大値をbとする
-		if(max < b){ max = b; }
+	public static float max(float firstNumber, float secondNumber){
+		// 最大値を仮にfirstNumberとする
+		float max = firstNumber;
+		// 最大値よりsecondNumberが大きければ最大値をsecondNumberとする
+		if(max < secondNumber){ max = secondNumber; }
 		// 最大値を返す
 		return max;
 	}
@@ -266,15 +266,15 @@ public final class e10_03_MinMax {
 	 * 二値の最大値を求める(double型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a double型整数値a
-	 * @param b double型整数値b
+	 * @param firstNumber double型整数値firstNumber
+	 * @param secondNumber double型整数値secondNumber
 	 * @return double 二値の最小値
 	 */
-	public static double max(double a, double b){
-		// 最大値を仮にaとする
-		double max = a;
-		// 最大値よりbが大きければ最大値をbとする
-		if(max < b){ max = b; }
+	public static double max(double firstNumber, double secondNumber){
+		// 最大値を仮にfirstNumberとする
+		double max = firstNumber;
+		// 最大値よりsecondNumberが大きければ最大値をsecondNumberとする
+		if(max < secondNumber){ max = secondNumber; }
 		// 最大値を返す
 		return max;
 	}
@@ -282,16 +282,16 @@ public final class e10_03_MinMax {
 	 * 三値の最大値を求める(int型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a int型整数値a
-	 * @param b int型整数値b
-	 * @param c int型整数値c
+	 * @param firstNumber int型整数値firstNumber
+	 * @param secondNumber int型整数値secondNumber
+	 * @param thirdNumber int型整数値thirdNumber
 	 * @return int 三値の最小値
 	 */
-	public static int max(int a, int b, int c){
+	public static int max(int firstNumber, int secondNumber, int thirdNumber){
 		// 二値の最大値を求める
-		int max = max(a, b);
-		// 最大値よりcが大きければ最大値をcとする
-		if(max < c){ max = c; }
+		int max = max(firstNumber, secondNumber);
+		// 最大値よりthirdNumberが大きければ最大値をthirdNumberとする
+		if(max < thirdNumber){ max = thirdNumber; }
 		// 最大値を返す
 		return max;
 	}
@@ -299,16 +299,16 @@ public final class e10_03_MinMax {
 	 * 三値の最大値を求める(long型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a long型整数値a
-	 * @param b long型整数値b
-	 * @param c long型整数値c
+	 * @param firstNumber long型整数値firstNumber
+	 * @param secondNumber long型整数値secondNumber
+	 * @param thirdNumber long型整数値thirdNumber
 	 * @return long 三値の最小値
 	 */
-	public static long max(long a, long b, long c){
+	public static long max(long firstNumber, long secondNumber, long thirdNumber){
 		// 二値の最大値を求める
-		long max = max(a, b);
-		// 最大値よりcが大きければ最大値をcとする
-		if(max < c){ max = c; }
+		long max = max(firstNumber, secondNumber);
+		// 最大値よりthirdNumberが大きければ最大値をthirdNumberとする
+		if(max < thirdNumber){ max = thirdNumber; }
 		// 最大値を返す
 		return max;
 	}
@@ -316,16 +316,16 @@ public final class e10_03_MinMax {
 	 * 三値の最大値を求める(float型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a float型整数値a
-	 * @param b float型整数値b
-	 * @param c float型整数値c
+	 * @param firstNumber float型整数値firstNumber
+	 * @param secondNumber float型整数値secondNumber
+	 * @param thirdNumber float型整数値thirdNumber
 	 * @return float 三値の最小値
 	 */
-	public static float max(float a, float b, float c){
+	public static float max(float firstNumber, float secondNumber, float thirdNumber){
 		// 二値の最大値を求める
-		float max = max(a, b);
-		// 最大値よりcが大きければ最大値をcとする
-		if(max < c){ max = c; }
+		float max = max(firstNumber, secondNumber);
+		// 最大値よりthirdNumberが大きければ最大値をthirdNumberとする
+		if(max < thirdNumber){ max = thirdNumber; }
 		// 最大値を返す
 		return max;
 	}
@@ -333,16 +333,16 @@ public final class e10_03_MinMax {
 	 * 三値の最大値を求める(double型)
 	 * @author t.hosoya
 	 * @since 20190624
-	 * @param a double型整数値a
-	 * @param b double型整数値b
-	 * @param c double型整数値c
+	 * @param firstNumber double型整数値firstNumber
+	 * @param secondNumber double型整数値secondNumber
+	 * @param thirdNumber double型整数値thirdNumber
 	 * @return double 三値の最小値
 	 */
-	public static double max(double a, double b, double c){
+	public static double max(double firstNumber, double secondNumber, double thirdNumber){
 		// 二値の最大値を求める
-		double max = max(a, b);
-		// 最大値よりcが大きければ最大値をcとする
-		if(max < c){ max = c; }
+		double max = max(firstNumber, secondNumber);
+		// 最大値よりthirdNumberが大きければ最大値をthirdNumberとする
+		if(max < thirdNumber){ max = thirdNumber; }
 		// 最大値を返す
 		return max;
 	}
