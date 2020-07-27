65fcd46d211aa510ca890b5573adc7a903a0f0c9
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2020-07-27 20:47:08 +0900

    2020/07/27_演習12-2_3回目_進捗率100％_指摘事項に対する修正

--- meikai_java/src/e12/e12_02_Tester.java
+++ meikai_java/src/e12/e12_02_Tester.java
@@ -17,10 +17,8 @@ public class e12_02_Tester {
 		e12_02_Account nakata = new e12_02_Account("仲田", "654321", 200);
 		// 比較する文言を表示
 		System.out.printf("%sと%sの普通預金と定期預金残高の合計額を比較\n\n", adachi.getName(), nakata.getName());
-		// 足立君の口座合計額と仲田君の口座合計額のどちらが多いか比較する
-		int num = comBalance(adachi, nakata);
-		// 比較結果に応じて文言を表示する
-		comResult(num, adachi, nakata);
+		// 足立君の口座合計額と仲田君の口座合計額のどちらが多いか比較し比較結果に応じて文言を表示する
+		comResult(adachi, nakata);
 
 		// TimeAccountクラスで口座開設
 		e12_02_TimeAccount tanaka = new e12_02_TimeAccount("田中", "111222", 1300, 7800);
@@ -28,83 +26,90 @@ public class e12_02_Tester {
 		e12_02_TimeAccount suzuki = new e12_02_TimeAccount("鈴木", "333444", 500, 90000);
 		// 比較する文言を表示
 		System.out.printf("%sと%sの普通預金と定期預金残高の合計額を比較\n\n", tanaka.getName(), suzuki.getName());
-		// 田中君の口座合計額と鈴木君の口座合計額のどちらが多いか比較する
-		int num2 = comBalance(tanaka, suzuki);
-		// 比較結果に応じて文言を表示する
-		comResult(num2, tanaka, suzuki);
+		// 田中君の口座合計額と鈴木君の口座合計額のどちらが多いか比較し比較結果に応じて文言を表示する
+		comResult(tanaka, suzuki);
 	}
 	/**
 	 * 比較結果に応じて文言を表示する
 	 * @author t.hosoya
 	 * @since 20190628
-	 * @param num comBalanceメソッドの判定結果
-	 * @param a 定期預金付き銀行口座クラス型変数
-	 * @param b 定期預金付き銀行口座クラス型変数
+	 * @param firstAccount 定期預金付き銀行口座クラス型変数
+	 * @param secondAccount 定期預金付き銀行口座クラス型変数
 	 */
-	public static void comResult(int num, e12_02_Account a, e12_02_Account b){
+	public static void comResult(e12_02_Account firstAccount, e12_02_Account secondAccount){
 		// comBalanceの結果を判定する
+		int num = comBalance(firstAccount, secondAccount);
 		switch(num){
 			// 返り値が1の場合
 			case 1:
-				// aの合計額が多いという文言を表示をする
-				System.out.printf("%sの口座合計額が%sの口座合計額に比べて多いです。\n\n", a.getName(), b.getName());
+				// firstAccountの合計額が多いという文言を表示をする
+				System.out.printf("%sの口座合計額が%sの口座合計額に比べて多いです。\n\n", firstAccount.getName(), secondAccount.getName());
 				break;
 			// 返り値が0の場合
 			case 0:
-				// aとbの合計額が同じという文言を表示をする
-				System.out.printf("%sと%sの口座合計額が同じです。\n\n", a.getName(), b.getName());
+				// firstAccountとsecondAccountの合計額が同じという文言を表示をする
+				System.out.printf("%sと%sの口座合計額が同じです。\n\n", firstAccount.getName(), secondAccount.getName());
 				break;
 			// 返り値が-1の場合
 			case -1:
-				// bの合計額が多いという文言を表示をする
-				System.out.printf("%sの口座合計額に比べて%s口座合計額が多いです。\n\n", a.getName(), b.getName());
+				// secondAccountの合計額が多いという文言を表示をする
+				System.out.printf("%sの口座合計額に比べて%s口座合計額が多いです。\n\n", firstAccount.getName(), secondAccount.getName());
 				break;
 		}
 	}
 
 	/**
-	 * 定期預金付き銀行口座クラス型変数a,bの普通預金と定期預金残高の合計額を比較した結果を返却
+	 * 定期預金付き銀行口座クラス型変数firstAccount,secondAccountの普通預金と定期預金残高の合計額を比較した結果を返却
 	 * @author t.hosoya
 	 * @since 20190628
-	 * @param a 定期預金付き銀行口座クラス型変数
-	 * @param b 定期預金付き銀行口座クラス型変数
+	 * @param firstAccount 定期預金付き銀行口座クラス型変数
+	 * @param secondAccount 定期預金付き銀行口座クラス型変数
 	 * @return 普通預金と定期預金残高の合計額を比較した結果
 	 */
-	public static int comBalance(e12_02_Account a, e12_02_Account b){
+	public static int comBalance(e12_02_Account firstAccount, e12_02_Account secondAccount){
+		//返り値
+		int result;
 		// 返り値を定数化
-		// aの合計額のほうが大きい場合
+		// firstAccountの合計額のほうが大きい場合
 		final int A_BIGGER = 1;
-		// aとbの合計額が同じ場合
+		// firstAccountとbの合計額が同じ場合
 		final int A_EQUAL_B = 0;
-		// bの合計額のほうが大きい場合
+		// secondAccountの合計額のほうが大きい場合
 		final int B_BIGGER = -1;
-		// aの合計額
+		// firstAccountの合計額
 		long totalA;
-		// bの合計額
+		// secondAccountの合計額
 		long totalB;
-		// aがAccount型のインスタンスの場合
-		if(a instanceof e12_02_TimeAccount){
+		// firstAccountがAccount型のインスタンスの場合
+		if(firstAccount instanceof e12_02_TimeAccount){
 			// aの普通預金と定期預金を合計額とする
-			totalA = ((e12_02_TimeAccount)a).getBalance() + ((e12_02_TimeAccount)a).getTimeBalance();
-		// aがTimeAccount型のインスタンスの場合
+			totalA = ((e12_02_TimeAccount)firstAccount).getBalance() + ((e12_02_TimeAccount)firstAccount).getTimeBalance();
+		// firstAccountがTimeAccount型のインスタンスの場合
+		}else{
+			// firstAccountの普通預金を合計額とする
+			totalA = firstAccount.getBalance();
+		}
+		// secondAccountがAccount型のインスタンスの場合
+		if(secondAccount instanceof e12_02_TimeAccount){
+			// secondAccountの普通預金と定期預金を合計額とする
+			totalB = ((e12_02_TimeAccount)secondAccount).getBalance() + ((e12_02_TimeAccount)secondAccount).getTimeBalance();
+		// secondAccountがTimeAccount型のインスタンスの場合
 		}else{
 			// aの普通預金を合計額とする
-			totalA = a.getBalance();
+			totalB = secondAccount.getBalance();
 		}
-		// bがAccount型のインスタンスの場合
-		if(b instanceof e12_02_TimeAccount){
-			// bの普通預金と定期預金を合計額とする
-			totalB = ((e12_02_TimeAccount)b).getBalance() + ((e12_02_TimeAccount)b).getTimeBalance();
-		// bがTimeAccount型のインスタンスの場合
-		}else{
-			// aの普通預金を合計額とする
-			totalB = b.getBalance();
+		// 合計額を比較値してfirstAccountのほうが多ければ1を返却
+		if(totalA > totalB){
+			result = A_BIGGER;
 		}
-		// 合計額を比較値してaのほうが多ければ1を返却
-		if(totalA > totalB){ return A_BIGGER; }
-		// 合計額を比較値してaとbを0を返却
-		else if(totalA == totalB){ return A_EQUAL_B; }
-		// 合計額を比較値してbのほうが多ければ-1を返却
-		else{ return B_BIGGER; }
+		// 合計額を比較値してfirstAccountとsecondAccountが等しければを0を返却
+		else if(totalA == totalB){
+			result = A_EQUAL_B;
+		}
+		// 合計額を比較値してsecondAccountのほうが多ければ-1を返却
+		else{
+			result = B_BIGGER;
+		}
+		return result;
 	}
 }
