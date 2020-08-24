90f486c9d934e5b53507f63e7c429cc449cf70cd
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2020-08-24 20:42:58 +0900

    2020/08/24_演習13-2_3回目_進捗率100％_指摘事項に対する修正

--- meikai_java/src/e13/e13_02_Tester.java
+++ meikai_java/src/e13/e13_02_Tester.java
@@ -15,9 +15,11 @@ public class e13_02_Tester {
 		System.out.print("辺の長さを入力してください：");
 
 		// 調整値を定数化
-		final int ADJUST_INT = 1;
+		final int SHAPE_MIN = 1;
 		// 図形の種類
-		final int SHAPE_TYPE = 4;
+		final int SHAPE_MAX = 4;
+		// 調整値を定数化
+		final int ADJUST_INT = 1;
 		// 辺の長さとする
 		int length = stdIn.nextInt();
 		// 直角の位置
@@ -27,31 +29,41 @@ public class e13_02_Tester {
 			System.out.print("直角の位置を選択してください（1...左下／2...左上／3...右下／4...右上）：");
 			// 入力値を直角の位置とする
 			anglePosition = stdIn.nextInt();
-		}while(anglePosition < ADJUST_INT && anglePosition > SHAPE_TYPE );
+		}while(anglePosition < SHAPE_MIN && anglePosition > SHAPE_MAX );
 
-		e13_01_Shape shape[] = new e13_01_Shape[SHAPE_TYPE - ADJUST_INT];
+		// 直角の位置を配列の引数に渡すために調整した変数
+		int displayAnglePosition = anglePosition - ADJUST_INT;
+
+		//e13_01_Shape shape[] = new e13_01_Shape[displayAnglePosition];
 
 
 		// 図形クラスをインスタンス化
 		switch(anglePosition){
-			// 1左下が選択された場合
-			case 1:
-				shape[anglePosition - ADJUST_INT] = new e13_02_PositionBottomLeft(length, anglePosition);
-				break;
-			// 2左上が選択された場合
-			case 2:
-				shape[anglePosition - ADJUST_INT] = new e13_02_PositionUpperLeft(length, anglePosition);
-				break;
-			// 3右下が選択された場合
-			case 3:
-				shape[anglePosition - ADJUST_INT] = new e13_02_PositionBottomRight(length, anglePosition);
-				break;
-			// 4右上が選択された場合
-			case 4:
-				shape[anglePosition - ADJUST_INT] = new e13_02_PositionUpperRight(length, anglePosition);
-				break;
-		}
-		// 生成した図形を描画する
-		shape[anglePosition - ADJUST_INT].print();
+				// 1左下が選択された場合
+				case 1:
+					e13_01_Shape positionBottomLeft = new e13_02_PositionBottomLeft(length, displayAnglePosition);
+					// 生成した図形を描画する
+					positionBottomLeft.print();
+					break;
+				// 2左上が選択された場合
+				case 2:
+					e13_01_Shape positionUpperLeft = new e13_02_PositionUpperLeft(length, displayAnglePosition);
+					// 生成した図形を描画する
+					positionUpperLeft.print();
+					break;
+				// 3右下が選択された場合
+				case 3:
+					e13_01_Shape positionBottomRight = new e13_02_PositionBottomRight(length, displayAnglePosition);
+					// 生成した図形を描画する
+					positionBottomRight.print();
+					break;
+				// 4右上が選択された場合
+				case 4:
+					e13_01_Shape positionUpperRight = new e13_02_PositionUpperRight(length, displayAnglePosition);
+					// 生成した図形を描画する
+					positionUpperRight.print();
+					break;
+			}
+
 	}
 }
