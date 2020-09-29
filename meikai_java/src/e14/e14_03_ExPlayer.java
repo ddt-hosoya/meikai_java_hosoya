512c1b7b1feb4ed877cd7c3a294d58301d9d2ae4
hosoya_t_ddt <ddt.hosoya.study@gmail.com>
2020-09-29 17:08:32 +0900

    2020/09/29_演習14-3_1回目_進捗率100％

---
+++ meikai_java/src/e14/e14_03_ExPlayer.java
@@ -0,0 +1,10 @@
+package e14;
+/**
+ * 拡張プレーヤー インターフェース（スロー再生付き）（p.467のコピー）
+ * @author t.hosoya
+ * @since 20200929
+ */
+public interface e14_03_ExPlayer extends e14_03_Player {
+	// スロー再生
+	void slow();
+}
---
+++ meikai_java/src/e14/e14_03_Player.java
@@ -0,0 +1,14 @@
+package e14;
+/**
+ * プレーヤー インターフェース（p.456のコピー）
+ * @author t.hosoya
+ * @since 20200929
+ *
+ */
+public interface e14_03_Player {
+	// 再生
+	void play();
+	// 停止
+	void stop();
+
+}
